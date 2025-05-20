1055e4a038b1234ad5c3614873da30de4dbea6e4
Cauak <caua.crosati@edu.unifil.br>
2025-05-20 20:12:52 -0300

    Entrega Feita dia 20/05/2025
    Entrega sobre mostrar os nós da Árvore Binária disponibilizada em sala em "Em-Ordem"

---
+++ Arvore_Binaria_Em_Ordem/ArvoreBinaria.java
@@ -0,0 +1,38 @@
+public class ArvoreBinaria {
+    No raiz;
+
+    public ArvoreBinaria() {
+        construirArvore();
+    }
+
+    private void construirArvore() {
+        No A = new No("A");
+        No B = new No("B");
+        No C = new No("C");
+        No D = new No("D");
+        No E = new No("E");
+        No F = new No("F");
+
+        A.esquerda = B;
+        A.direita = C;
+
+        B.esquerda = D;
+        B.direita = E;
+
+        C.direita = F;
+
+        this.raiz = A;
+    }  
+
+    public void emOrdem(No no) {
+        if (no != null) {
+            emOrdem(no.esquerda);
+            System.out.print(no.valor + " ");
+            emOrdem(no.direita);
+        }
+    }
+
+    public void imprimirEmOrdem() {
+        emOrdem(raiz);
+    }
+}
