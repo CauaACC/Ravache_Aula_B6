080617f75ae09f67ab554256d20ac0aabf848e64
Cauak <caua.crosati@edu.unifil.br>
2025-05-20 19:45:37 -0300

    Entrega feita dia 20-05-2025
    Entrega sobre fazer um código em java para contar todos os nós que habitam a árvore binária disponibilizada em sala de aula.

---
+++ Arvore_binaria_contar_nos/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ArvoreBinaria arvore = new ArvoreBinaria();
+        int totalNos = arvore.contarNos();
+        System.out.println("Total de nós na árvore binária: " + totalNos);
+    }
+}
